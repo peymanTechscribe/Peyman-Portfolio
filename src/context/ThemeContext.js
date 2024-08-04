82373ae8b8675b035d15a63646d073d3ce38e49e
AlirezaAdabi <alireza.adabi78@gmail.com>
2024-08-04 12:38:06 +0330

    fix: default theme

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -7,7 +7,7 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
 
   const toggleTheme = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark';
